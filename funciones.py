84951fd0813ffc0b5e04946b9133930b936527cc
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-10-02 18:00:10 +0000

    modelo constructor 1

--- funciones.py
+++ funciones.py
@@ -19,3 +19,23 @@ def rango(d):
     d = np.multiply(d, 0.89) 
     d = np.add(d, 0.01)
     return d
+
+
+
+
+def modelo_constructor_1(input_dim,hidden_dim,output_dim):
+    model = Sequential()
+    model.add(Dense_Co(output_dim,hidden_dim=hidden_dim ,input_dim=input_dim))
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+    return model
+def modelo_constructor(input_dim,hidden_dim,output_dim):
+    model = Sequential()
+    model.add(Dense_Co(output_dim,hidden_dim=hidden_dim ,input_dim=input_dim,  kernel_initializer=RandomNormal(
+            mean=0.0, stddev=0.04), bias_initializer=RandomNormal(mean=0.0, stddev=0.04)))
+    model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+    return model
+
+epochs=5
+batch_size=1
+kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+cvscores = []
