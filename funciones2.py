15131bf880d5cb67bec62910c31170d2c18773a3
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-10-02 18:10:47 +0000

    primeras dos graficas

--- funciones2.py
+++ funciones2.py
@@ -3,7 +3,11 @@ from keras.models import Model, Sequential
 from keras.layers import Input, Embedding, LSTM, Dense, concatenate, Dropout, Flatten, Conv2D, MaxPool2D
 from keras.utils.np_utils import to_categorical
 from keras.optimizers import RMSprop, adam
-from keras.prepro
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ReduceLROnPlateau
+from keras import backend as K
+from keras.optimizers import SGD
+from ke
 
 
 
@@ -48,7 +52,7 @@ def exp_(x):
 def log_(x):                                        
     return  np.log(x)
 
-def generar_random( shape_1,shape_2):
+def generate_random( shape_1,shape_2):
     temp= np.random.random(shape_1*shape_2)
     temp1=temp.reshape(shape_1,shape_2)
     return temp1
@@ -61,3 +65,83 @@ def fun_2(x):
 
 def fun_3(x):
     return 22*power_(x, 20)+sigmoid_array(x)+2*exp_(x)+5*log_(x)
+  
+
+#Graficas de las funciones basandonos en el paper dado
+def plot_loss(size,in_1,in_2,in_3,in_4,in_5,in_6):
+
+    plt.rcParams.update({'font.size': 16})
+
+    plt.figure(figsize=(20,10))
+    plt.subplot(231)
+    training_loss1 = in_1.history['loss']
+    test_loss1 = in_1.history['val_loss']
+    epoch_count = range(1, len(training_loss1) + 1)
+    plt.plot(epoch_count, training_loss1, 'r--')
+    plt.plot(epoch_count, test_loss1, 'b-')
+    plt.legend(['Training Loss', 'Test Loss'])
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.ylim(0, size)
+    plt.title('Architure 1')
+
+
+    plt.subplot(232)
+    training_loss1 = in_2.history['loss']
+    test_loss1 = in_2.history['val_loss']
+    epoch_count = range(1, len(training_loss1) + 1)
+    plt.plot(epoch_count, training_loss1, 'r--')
+    plt.plot(epoch_count, test_loss1, 'b-')
+    plt.legend(['Training Loss', 'Test Loss'])
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.ylim(0, size)
+    plt.title('Architure 2')
+
+
+
+
+
+def plot_shape(in_1,in_2,in_3,in_4,in_5,in_6):
+
+    plt.figure(figsize=(20,10))
+    plt.rcParams.update({'font.size': 16})
+
+    plt.subplot(231)
+    plt.scatter(in_1[0], in_1[1])
+    plt.scatter(in_1[0], in_1[2])
+    plt.title('Architure 1')
+
+
+    plt.subplot(232)
+    plt.scatter(in_2[0], in_2[1])
+    plt.scatter(in_2[0], in_2[2])
+    plt.title('Architure 2')
+
+
+    plt.subplot(233)
+    plt.scatter(in_3[0], in_3[1])
+    plt.scatter(in_3[0], in_3[2])
+    plt.title('Architure 3')
+
+
+    plt.subplot(234)
+    plt.scatter(in_4[0], in_4[1])
+    plt.scatter(in_4[0], in_4[2])
+    plt.title('Architure 4')
+    
+    plt.subplot(235)
+    plt.scatter(in_5[0], in_5[1])
+    plt.scatter(in_5[0], in_5[2])
+    plt.title('Architure 5')    
+    
+    plt.subplot(236)
+    plt.scatter(in_6[0], in_6[1])
+    plt.scatter(in_6[0], in_6[2])
+    plt.title('Our Architure')
+
+
+    plt.subplots_adjust(top=0.92, bottom=0.08, left=0.0010, right=0.95, hspace=0.3,
+                        wspace=0.18)
+
+    plt.show()
