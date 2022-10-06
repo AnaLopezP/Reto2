ae9a4b10cd19b9223c015a1eed8889e68836d1d4
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-06 20:33:11 +0200

    información básica y organización del csv

--- diabetes.py
+++ diabetes.py
@@ -2,3 +2,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+#leemos csv
+diabetes = pd.read_csv('diabetes.csv')
+
+#columnas
+print(diabetes.columns )
+print('\n')
+#dimensión
+print('dimensión diabetes: ' + str(diabetes.shape) + '\n')
+#Agrupamos datos
+print(diabetes.groupby('Datos agrupados').size())
